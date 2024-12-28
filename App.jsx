9c176a2c04c5cbf184d0a2bc00734f3155518e2d
2041997 <arktiniinnovations.com>
2024-12-28 21:59:52 +0530

    Initial commit for Nativewind-Starter

--- App.jsx
+++ App.jsx
@@ -6,7 +6,7 @@ import './global.css'
 export default function App() {
   return (
     <View className='flex-1 bg-black items-center justify-center'>
-      <Text className='font-bold text-white text-xl'>Open up App.jsx to start working on your app!</Text>
+      <Text className='font-bold text-white text-3xl'>Open up App.jsx to start working on your app!</Text>
       <StatusBar style="light" />
     </View>
   );
