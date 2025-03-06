aad1decb8889d56f951a1c81e8610f51b2d213f9
devarktini <arktiniinnovations.com>
2025-03-06 17:29:07 +0530

    code added to the input path will be executed before the main code

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-import 'react-native-get-random-values'; // Import this first if using UUID or related libraries
-if (typeof global.setImmediate === 'undefined') {
-    global.setImmediate = (fn, ...args) => setTimeout(fn, 0, ...args);
-}
+import { registerRootComponent } from 'expo';
+
+import App from './App';
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
